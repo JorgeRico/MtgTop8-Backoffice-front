48b0c22089f13d4820d26bda8513efd426ebc4e2
JorgeRico <jorge.rico.garriga@gmail.com>
2026-02-13 14:44:57 +0100

    hotfix: rename extension files

---
+++ src/types/web-routing.tsx
@@ -0,0 +1,13 @@
+export const routing = {
+    'leagues'     : '/leagues',
+    'tournaments' : '/tournaments',
+    'decks'       : '/decks',
+    'players'     : '/players',
+    'cards'       : '/cards',
+    'profile'     : '/profile',
+    'dashboard'   : '/dashboard',
+    'signup'      : '/signup',
+    'settings'    : '/settings',
+    'home'        : '/',
+    'recover'     : '/recover'
+}
